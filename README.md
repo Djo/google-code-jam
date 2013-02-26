337bb931d3902fceca6d0acfc0b9553799a69085
Andrew Djoga <andrew.djoga@gmail.com>
2013-02-26 19:25:20 +0200

    Minimum Scalar Product problem

---
+++ README.md
@@ -0,0 +1,4 @@
+### Some my solutions of [Google Code Jam](http://code.google.com/codejam) problems.
+
+* Code Jam 2008, Round 1A: [Minimum Scalar Product](http://code.google.com/codejam/contest/dashboard?c=32016#s=p0)
+* more soon..
---
+++ minimum_scalar_product/task.rb
@@ -0,0 +1,31 @@
+# Problem A. Minimum Scalar Product,
+# http://code.google.com/codejam/contest/dashboard?c=32016#s=p0
+#
+# Use ruby 1.9.3, pass the input file as the first argument:
+# ruby task.rb A-small-practice.in
+
+def scalar_product(vector1, vector2)
+  vector1.sort!
+  vector2.sort! { |x, y| y <=> x }
+  product = 0
+  vector1.each_with_index do |number1, index|
+    product += number1 * vector2[index]
+  end
+
+  product
+end
+
+file_in = ARGV[0]
+file_out = file_in.sub('.in', '.out')
+File.open(file_in, 'r') do |input|
+  File.open(file_out, 'w') do |output|
+    test_cases = input.readline.to_i
+    1.upto(test_cases) do |test_case|
+      input.readline
+      vector1 = input.readline.split(' ').map(&:to_i)
+      vector2 = input.readline.split(' ').map(&:to_i)
+      product = scalar_product(vector1, vector2)
+      output << "Case ##{test_case}: #{product}\n"
+    end
+  end
+end
