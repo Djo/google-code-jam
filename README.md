27dadf956313a87acf9b403b307138a7651bed56
Andrew Djoga <andrew.djoga@gmail.com>
2013-02-27 20:01:00 +0200

    Reverse Words problem

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ### Some my solutions of [Google Code Jam](http://code.google.com/codejam) problems.
 
 * Code Jam 2008, Round 1A: [Minimum Scalar Product](http://code.google.com/codejam/contest/dashboard?c=32016#s=p0)
+* Africa 2010, Qualification Round: [Reverse Words](https://code.google.com/codejam/contest/351101/dashboard#s=p1)
 * more soon..
---
+++ reverse_words/task.rb
@@ -0,0 +1,18 @@
+# Problem B. Reverse Words,
+# https://code.google.com/codejam/contest/dashboard?c=351101#s=p1
+#
+# Use ruby 1.9.3, pass the input file as the first argument:
+# ruby task.rb B-small-practice.in
+
+file_in = ARGV[0]
+file_out = file_in.sub('.in', '.out')
+File.open(file_in, 'r') do |input|
+  File.open(file_out, 'w') do |output|
+    test_cases = input.readline.to_i
+    1.upto(test_cases) do |test_case|
+      words = input.readline.split(' ')
+      reversed = words.reverse.join(' ')
+      output << "Case ##{test_case}: #{reversed}\n"
+    end
+  end
+end
