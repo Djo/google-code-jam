3f2feb7517d8ca9b1f1d9206f6c5a857fda5fc61
Andrew Djoga <andrew.djoga@gmail.com>
2013-03-04 21:35:11 +0200

    Rope Intranet problem

--- README.md
+++ README.md
@@ -4,4 +4,5 @@
 * Africa 2010, Qualification Round: [Reverse Words](https://code.google.com/codejam/contest/351101/dashboard#s=p1)
 * Code Jam 2010, Round 1B: [File Fix-it](https://code.google.com/codejam/contest/635101/dashboard#s=p0)
 * Code Jam 2010, Round 1A: [Rotate](http://code.google.com/codejam/contest/544101/dashboard#s=p0)
+* Code Jam 2010, Round 1C: [Rope Intranet](http://code.google.com/codejam/contest/619102/dashboard#s=p0)
 * more soon..
---
+++ rope_intranet/task.rb
@@ -0,0 +1,31 @@
+# Problem A. Rope Intranet,
+# https://code.google.com/codejam/contest/619102/dashboard#s=p0
+#
+# Use ruby 1.9.3, pass the input file as the first argument:
+# ruby task.rb A-small-practice.in
+
+def intersection_points(wires)
+  wires.sort_by! { |a, _| a }
+  points = 0
+  0.upto(wires.length - 2) do |i|
+    (i + 1).upto(wires.length - 1) do |j|
+      points += 1 if wires[i][1] > wires[j][1]
+    end
+  end
+
+  points
+end
+
+file_in = ARGV[0]
+file_out = file_in.sub('.in', '.out')
+File.open(file_in, 'r') do |input|
+  File.open(file_out, 'w') do |output|
+    test_cases = input.readline.to_i
+    1.upto(test_cases) do |test_case|
+      wires_count = input.readline.to_i
+      wires = []
+      wires_count.times { wires << input.readline.split(' ').map(&:to_i) }
+      output << "Case ##{test_case}: #{intersection_points wires}\n"
+    end
+  end
+end
